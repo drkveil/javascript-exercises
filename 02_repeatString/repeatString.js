5011248bb9c7a7960ff1dd48e56dbe5f12447cc5
Suleiman Yahaya <132784747+drkveil@users.noreply.github.com>
2025-01-27 11:14:48 +0100

    Solve 02 repeatSring

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+let string = "";
+let i = 1;
 
+const repeatString = function() {
+    while (i <= 3){
+        string += "hey";
+        i++;
+    }
+    return string;
 };
 
 // Do not edit below this line
