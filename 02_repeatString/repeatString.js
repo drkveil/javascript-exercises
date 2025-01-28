a303eeef0cbe7501da5aa26594085df11d9d4d04
Suleiman Yahaya <132784747+drkveil@users.noreply.github.com>
2025-01-28 13:05:17 +0100

    02 Solve and pass all tests
    All tests passed successfully

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
 const repeatString = function(string, num) {
     addString = "";
+    if (num < 0){
+        return "ERROR";
+    }else if (string === null){
+        return null;
+    }
     for (let i = 1; i <= num; i++) {
         addString += string;
     }
