077e4e1aedc96ae73abbd8134b7ba493c6128436
Suleiman Yahaya <132784747+drkveil@users.noreply.github.com>
2025-02-02 03:22:30 +0100

    Solved 04 removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toRemove) {
+    //Filter loops through array and includes checkes if numberis in romoveItem
+    return array.filter(removeItem => !toRemove.includes(removeItem));
 };
 
 // Do not edit below this line
