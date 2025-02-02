938ea057e1bcadc15a36ab3498d58971c24c25c1
Suleiman Yahaya <132784747+drkveil@users.noreply.github.com>
2025-02-02 03:56:03 +0100

    Wrong solution to sumAll
    This adds up numbers in an array and output the answer

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(...numbers) {
+    let sum =  0;
+    for (i = 0; i < numbers.length; i++){
+        sum += numbers[i];
+    }
+    return sum;
 };
+
+console.log(sumAll(2, 3, 5, 7, 3, 5));
 
 // Do not edit below this line
 module.exports = sumAll;
