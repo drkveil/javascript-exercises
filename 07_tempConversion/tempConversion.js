de5137cb9628cd90d336b61769148fb0cd481585
Suleiman Yahaya <132784747+drkveil@users.noreply.github.com>
2025-02-07 14:06:44 +0100

    Complete Arrays and Loop

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+// Formula C = (°F - 32) × 5/9
+const convertToCelsius = function(input) {
+    let convert = (input - 32 ) * 5/9;
+    const converted = Number(convert.toFixed(1));
+    return converted;
 };
 
-const convertToFahrenheit = function() {
+console.log()
+
+// Formula F = (°C × 9/5) + 32
+const convertToFahrenheit = function(input) {
+    let convert = (input * 9/5 ) + 32;
+    const converted = Number(convert.toFixed(1));
+    return converted;
 };
 
 // Do not edit below this line
